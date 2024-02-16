2a33aa00591ccf049f46e5f2cbbcce7471464357
ajr250571 <ajr250571@gmail.com>
2024-02-16 15:12:13 -0300

    First Commit

---
+++ core/erp/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+from datetime import datetime
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Nombre")
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    names = models.CharField(max_length=150, verbose_name="Nombres")
+    dni = models.CharField(max_length=10, verbose_name="DNI", unique=True)
+    date_joined = models.DateField(
+        default=datetime.now, verbose_name="Fecha Registro")
+    date_created = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha Creacion")
+    date_updated = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha Actualizacion")
+    age = models.PositiveIntegerField(
+        default=0, verbose_name="Edad")
+    salary = models.DecimalField(
+        max_digits=14, decimal_places=2, default=0.00, verbose_name="Sueldo")
+    state = models.BooleanField(default=True, verbose_name="Estado")
+    avatar = models.ImageField(
+        upload_to="avatar/%Y/%m/%d", null=True, blank=True)
+    cvitae = models.FileField(
+        upload_to="cvitae/%Y/%m/%d", null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Empleado'
+        verbose_name_plural = 'Empleados'
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.names
