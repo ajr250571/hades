0339848d19afa9a1d539a8f672c0097f82619d21
ajr250571 <ajr250571@gmail.com>
2024-02-16 20:23:50 -0300

    graba

--- core/erp/models.py
+++ core/erp/models.py
@@ -8,8 +8,8 @@ class Type(models.Model):
     name = models.CharField(max_length=150, verbose_name="Nombre")
 
     class Meta:
-        verbose_name = "Tipo"
-        verbose_name_plural = "Tipos"
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
         ordering = ["id"]
 
     def __str__(self):
@@ -19,23 +19,27 @@ class Type(models.Model):
 class Employee(models.Model):
     names = models.CharField(max_length=150, verbose_name="Nombres")
     dni = models.CharField(max_length=10, verbose_name="DNI", unique=True)
-    date_joined = models.DateField(default=datetime.now, verbose_name="Fecha Registro")
-    date_created = models.DateTimeField(auto_now=True, verbose_name="Fecha Creacion")
+    date_joined = models.DateField(
+        default=datetime.now, verbose_name="Fecha Registro")
+    date_created = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha Creacion")
     date_updated = models.DateTimeField(
-        auto_now_add=True, verbose_name="Fecha Actualizacion"
-    )
-    age = models.PositiveIntegerField(default=0, verbose_name="Edad")
+        auto_now_add=True, verbose_name="Fecha Actualizacion")
+    age = models.PositiveIntegerField(
+        default=0, verbose_name="Edad")
     salary = models.DecimalField(
-        max_digits=14, decimal_places=2, default=0.00, verbose_name="Sueldo"
-    )
+        max_digits=14, decimal_places=2, default=0.00, verbose_name="Sueldo")
     state = models.BooleanField(default=True, verbose_name="Estado")
-    avatar = models.ImageField(upload_to="avatar/%Y/%m/%d", null=True, blank=True)
-    cvitae = models.FileField(upload_to="cvitae/%Y/%m/%d", null=True, blank=True)
-    type = models.ForeignKey(Type, verbose_name="Tipo", on_delete=models.CASCADE)
+    avatar = models.ImageField(
+        upload_to="avatar/%Y/%m/%d", null=True, blank=True)
+    cvitae = models.FileField(
+        upload_to="cvitae/%Y/%m/%d", null=True, blank=True)
+    type = models.ForeignKey(Type, verbose_name="Tipo",
+                             on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = "Empleado"
-        verbose_name_plural = "Empleados"
+        verbose_name = 'Empleado'
+        verbose_name_plural = 'Empleados'
         ordering = ["id"]
 
     def __str__(self):
